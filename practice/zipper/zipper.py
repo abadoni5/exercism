7231d2db9cd9b24ff174b8cb23eddaf3dce24ca5
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2024-04-12 20:43:34 +0530

    zipper

--- practice/zipper/zipper.py
+++ practice/zipper/zipper.py
@@ -1,28 +1,53 @@
 class Zipper:
     @staticmethod
     def from_tree(tree):
-        pass
+        # Constructs a Zipper instance from a given tree.
+        return Zipper(dict(tree), [])
+
+    def __init__(self, tree, ancestors):
+        # Initializes a Zipper instance with a tree and ancestors.
+        self.tree = tree
+        self.ancestors = ancestors
 
     def value(self):
-        pass
+        # Returns the value of the current node.
+        return self.tree['value']
 
-    def set_value(self):
-        pass
+    def set_value(self, value):
+        # Sets the value of the current node.
+        self.tree['value'] = value
+        return self
 
     def left(self):
-        pass
+        # Moves to the left child node if it exists.
+        if self.tree['left'] is None:
+            return None
+        return Zipper(self.tree['left'], self.ancestors + [self.tree])
 
-    def set_left(self):
-        pass
+    def set_left(self, tree):
+        # Sets the left child of the current node.
+        self.tree['left'] = tree
+        return self
 
     def right(self):
-        pass
+        # Moves to the right child node if it exists.
+        if self.tree['right'] is None:
+            return None
+        return Zipper(self.tree['right'], self.ancestors + [self.tree])
 
-    def set_right(self):
-        pass
+    def set_right(self, tree):
+        # Sets the right child of the current node.
+        self.tree['right'] = tree
+        return self
 
     def up(self):
-        pass
+        # Moves to the parent node if it exists.
+        if not self.ancestors:
+            return None
+        return Zipper(self.ancestors[-1], self.ancestors[:-1])
 
     def to_tree(self):
-        pass
+        # Returns the root of the tree.
+        if any(self.ancestors):
+            return self.ancestors[0]
+        return self.tree
