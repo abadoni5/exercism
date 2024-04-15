5a8046897e2fb266a979b9581981ac38c6b78ceb
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2024-04-15 11:17:46 +0530

    hangman

--- practice/hangman/hangman.py
+++ practice/hangman/hangman.py
@@ -1,20 +1,61 @@
-# Game status categories
-# Change the values as you see fit
-STATUS_WIN = 'win'
-STATUS_LOSE = 'lose'
-STATUS_ONGOING = 'ongoing'
+"""
+Hangman game implementation using a class.
 
+The Hangman class encapsulates the logic for playing the Hangman game. It allows initializing the game with a word, guessing letters, and checking the game's status.
+
+Approach:
+- The class initializes with a word chosen for the game, setting attributes like remaining_guesses, status, and creating a masked version of the word.
+- During the game, players can guess letters. If the guessed letter is correct, it updates the masked word; otherwise, it decreases the remaining guesses.
+- The game ends either when the word is completely guessed (STATUS_WIN) or when the remaining guesses run out (STATUS_LOSE).
+
+"""
+STATUS_WIN = "win"
+STATUS_LOSE = "lose"
+STATUS_ONGOING = "ongoing"
 
 class Hangman:
-    def __init__(self, word):
+    """
+    Class for the Hangman game.
+    """
+    def __init__(self, word: str):
+        self.word = word
+        self._set_word = set(word)
         self.remaining_guesses = 9
         self.status = STATUS_ONGOING
-
-    def guess(self, char):
-        pass
-
-    def get_masked_word(self):
-        pass
-
-    def get_status(self):
-        pass
+        self._masked_word = [0] * len(self.word)
+        self._guessed: set[str] = set()
+ 
+    def guess(self, char: str) -> None:
+        """
+        Try to guess a letter.
+        """
+        if self.status == STATUS_ONGOING:
+            if char not in self._set_word or char in self._guessed:
+                self.remaining_guesses -= 1
+                if self.remaining_guesses < 0:
+                    self.status = STATUS_LOSE
+            else:
+                self._guessed.add(char)
+                self._masked_word = [
+                    value if self.word[i] != char else 1
+                    for i, value in enumerate(self._masked_word)
+                ]
+                if sum(self._masked_word) == len(self.word):
+                    self.status = STATUS_WIN
+        else:
+            raise ValueError("The game has already ended.")
+ 
+    def get_masked_word(self) -> str:
+        """
+        Return the masked word.
+        """
+        return "".join(
+            letter if self._masked_word[i] else "_"
+            for i, letter in enumerate(self.word)
+        )
+ 
+    def get_status(self) -> str:
+        """
+        Get the status of the game.
+        """
+        return self.status
