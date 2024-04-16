bd3666ec172da56d67c8a34db5e17113dad5cc82
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2024-04-16 21:32:52 +0530

    spiral matrix

--- practice/spiral-matrix/spiral_matrix.py
+++ practice/spiral-matrix/spiral_matrix.py
@@ -1,2 +1,30 @@
+"""
+Spiral Matrix - spiral_matrix generates a square matrix of a given size in a spiral pattern.
+It initializes a matrix with None values and then fills it with numbers in a spiral pattern.
+The function uses itertools.cycle to iterate through a cycle of movement directions:
+(0,1) for moving right, (1,0) for moving down, (0,-1) for moving left, and (-1,0) for moving up.
+The matrix is filled in a spiral pattern by updating the current cell's position and direction 
+based on the cycle and checking for boundaries and filled cells.
+"""
+ 
+from itertools import cycle
+ 
 def spiral_matrix(size):
-    pass
+    matrix = [[None] * size for _ in range(size)]
+    r, c = 0, 0
+    # this cycle determines the movement of the "current cell"
+    # (0,1) represents moving along a row to the right
+    # (1,0) represents moving down a column
+    deltas = cycle(((0,1), (1,0), (0,-1), (-1,0)))
+    dr, dc = next(deltas)
+    for i in range(size**2):
+        matrix[r][c] = i+1
+        if (
+            not 0 <= r+dr < size or
+            not 0 <= c+dc < size or
+            matrix[r+dr][c+dc] is not None
+        ):
+            dr, dc = next(deltas)
+        r += dr
+        c += dc
+    return matrix
